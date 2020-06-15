ccd2d725b275d8d5d9b6ee7e445cb1517c12a155
GEMEOSINFO <gemeos23@hotmail.com>
2020-06-15 17:28:38 -0300

    criamos a classe page

--- index.php
+++ index.php
@@ -2,19 +2,25 @@
 
 require_once("vendor/autoload.php");
 
+use \Hcode\Page;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new \Hcode\DB\Sql();
+	//$sql = new \Hcode\DB\Sql();
 
-	$results = $sql->select("SELECT * FROM tb_users");
+	//$results = $sql->select("SELECT * FROM tb_users");
 
-	echo json_encode($results);
+	//echo json_encode($results);
 
 	//echo "ok"; 
+
+	$page = new Page();
+
+	$page->setTpl("index");
 
 });
 
---
+++ views-cache/header.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -0,0 +1,7 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Hcode Store</title>
+	</head>
+	<body>
+		
