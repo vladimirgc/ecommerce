007ff78c3928cea30e190d3d480b4f9638dae18f
GEMEOSINFO <gemeos23@hotmail.com>
2020-07-15 15:36:13 -0300

    categoria vs produtos

--- index.php
+++ index.php
@@ -289,11 +289,13 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 });
 
 $app->get("/categories/:idcategory", function($idcategory){
+	//User::verifyLogin(); //chama método estático da classe User
 	$category = new Category();
 	$category->get((int)$idcategory); //necessario converte para inteiro por tudo que vem pela URL é texto
 	$page = new Page();
 	$page->setTpl("category", [
-		"category"=>$category->getValues()
+		"category"=>$category->getValues(),
+		"products"=>Product::checkList($category->getProducts())
 	]);
 });
 
@@ -355,6 +357,41 @@ $app->get("/admin/products/:idproduct/delete", function($idproduct){
 	$product->get((int)$idproduct);
 	$product->delete();
 	header("Location: /admin/products");
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory/products", function($idcategory){
+	User::verifyLogin(); //chama método estático da classe User
+	$category = new Category();
+	$category->get((int)$idcategory); //necessario converte para inteiro por tudo que vem pela URL é texto
+	$page = new PageAdmin();
+	$page->setTpl("categories-products",[
+		"category"=>$category->getValues(),
+		"productsRelated"=>$category->getProducts(),
+		"productsNotRelated"=>$category->getProducts(false)
+	]);
+
+});
+
+$app->get("/admin/categories/:idcategory/products/:idproduct/add", function($idcategory, $idproduct){
+	User::verifyLogin(); //chama método estático da classe User
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$category->addProduct($product);
+	header("Location: /admin/categories/".$idcategory."/products");
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory/products/:idproduct/remove", function($idcategory, $idproduct){
+	User::verifyLogin(); //chama método estático da classe User
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$category->removeProduct($product);
+	header("Location: /admin/categories/".$idcategory."/products");
 	exit;
 });
 
